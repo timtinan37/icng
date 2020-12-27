b0d61c94884631394116cdee1b4956a91b3eea7c
Tariq Imtinan <timtinan@outlook.com>
2020-12-27 17:57:37 +0600

    add login creds for demo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,13 +14,14 @@
                 <div class="card-body">
                   <h1>Login</h1>
                   <p class="text-muted">Sign In to your account</p>
+                  <i>Creds: admin@example.com, password = password</i>
                   <div class="input-group mb-3">
                     <div class="input-group-prepend"><span class="input-group-text">
                         <svg class="c-icon">
                           <use xlink:href="{{ asset('/coreui/vendors/@coreui/icons/svg/free.svg#cil-user') }}"></use>
                         </svg></span></div>
                     
-                    <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="admin@example.com" required autocomplete="email" autofocus>
                     
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -33,7 +34,7 @@
                         <svg class="c-icon">
                           <use xlink:href="{{ asset('/coreui/vendors/@coreui/icons/svg/free.svg#cil-lock-locked') }}"></use>
                         </svg></span></div>
-                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="password" required autocomplete="current-password">
 
                     @error('password')
                         <span class="invalid-feedback" role="alert">
